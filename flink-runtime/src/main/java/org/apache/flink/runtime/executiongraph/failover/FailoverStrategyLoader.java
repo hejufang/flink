ad87bccc872cf3e7cc74ce9cedcfd7ab44085f0a
zhangyunfan <zhangyunfan@bytedance.com>
2017-09-14 11:57:49 +0800

    Fix issure 19: Add failover strategy restart single task or node regardless the execute graph.
    Change-Id: I77c8a0ccfcfd99ed4615fab08a23ddb1d3699f29
    
    (cherry picked from commit 6e68f49)

--- flink-runtime/src/main/java/org/apache/flink/runtime/executiongraph/failover/FailoverStrategyLoader.java
+++ flink-runtime/src/main/java/org/apache/flink/runtime/executiongraph/failover/FailoverStrategyLoader.java
@@ -41,8 +41,8 @@ public class FailoverStrategyLoader {
 	/** Config name for the {@link AdaptedRestartPipelinedRegionStrategyNG}. */
 	public static final String PIPELINED_REGION_RESTART_STRATEGY_NAME = "region";
 
-	/** Config name for the {@link RestartPipelinedRegionStrategy}. */
-	public static final String LEGACY_PIPELINED_REGION_RESTART_STRATEGY_NAME = "region-legacy";
+	/** Config name for the {@link RestartSingleStrategy} */
+	public static final String SINGLE_RESTART_STRATEGY_NAME = "single";
 
 	// ------------------------------------------------------------------------
 
@@ -73,6 +73,9 @@ public class FailoverStrategyLoader {
 
 				case INDIVIDUAL_RESTART_STRATEGY_NAME:
 					return new RestartIndividualStrategy.Factory();
+
+				case SINGLE_RESTART_STRATEGY_NAME:
+					return new RestartSingleStrategy.Factory();
 
 				default:
 					// we could interpret the parameter as a factory class name and instantiate that
