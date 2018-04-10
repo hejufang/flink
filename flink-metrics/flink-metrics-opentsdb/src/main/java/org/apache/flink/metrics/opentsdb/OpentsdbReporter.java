461299e7761a183aface9bac7b2f13eaa2777939
zoudan <zoudan@bytedance.com>
2018-04-10 14:36:33 +0800

    metrics 兼容job name 和 source中包含.的情况
    Change-Id: I389be576fe52c466f60a4b251b0231d415f04e7a
    
    (cherry picked from commit 47042c8)

--- flink-metrics/flink-metrics-opentsdb/src/main/java/org/apache/flink/metrics/opentsdb/OpentsdbReporter.java
+++ flink-metrics/flink-metrics-opentsdb/src/main/java/org/apache/flink/metrics/opentsdb/OpentsdbReporter.java
@@ -45,7 +45,7 @@ import java.util.regex.Matcher;
 public class OpentsdbReporter extends AbstractReporter implements Scheduled {
 	private final static Logger LOG = LoggerFactory.getLogger(OpentsdbReporter.class);
 	private final static Pattern KAFKA_CONSUMER_PATTERN = Pattern.compile("taskmanager\\." +
-			"([^\\.]+)\\.([^\\.]+)\\.KafkaConsumer\\.([^\\.]+)\\.([^-]+)-(\\d+)");
+			"(.+)\\.KafkaConsumer\\.(.+)\\.([^-]+)-(\\d+)");
 	private final static Pattern JOB_MANAGER_PATTERN = Pattern.compile(
 			"(\\S+)\\.(jobmanager\\.\\S+)");
 	private final static Pattern TASK_MANAGER_PATTERN_1 = Pattern.compile(
@@ -207,13 +207,12 @@ public class OpentsdbReporter extends AbstractReporter implements Scheduled {
 		key = key.replace("..", ".");
 		Matcher matcher2 = KAFKA_CONSUMER_PATTERN.matcher(key);
 		if (matcher2.find()) {
-			String jobName = matcher2.group(1);
-			String sourceName = matcher2.group(2);
-			String quota = matcher2.group(3);
-			String topic = matcher2.group(4);
-			String partition = matcher2.group(5);
+			String jobAndSource = matcher2.group(1);
+			String quota = matcher2.group(2);
+			String topic = matcher2.group(3);
+			String partition = matcher2.group(4);
 			String taskManagerMetricName =
-					"taskmanager." + jobName + "." + sourceName + ".KafkaConsumer." + quota;
+					"taskmanager." + jobAndSource + ".KafkaConsumer." + quota;
 			tags.add(new TagKv("topic", topic));
 			tags.add(new TagKv("partition", partition));
 			taskManagerMetricName = taskManagerMetricName.replace("..", ".");
