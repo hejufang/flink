2f81e3baa83306853468c2f8a435ae3dbbefaa49
zoudan <zoudan@bytedance.com>
2019-01-10 20:00:28 +0800

    flink-python-byted: add topology
    Change-Id: I1f3fce6fb9ddf6077101061861872f02b7157809

---
+++ flink-contrib/flink-python-byted/src/main/java/com/bytedance/flink/topology/FlinkSubmitter.java
@@ -0,0 +1,221 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.bytedance.flink.topology;
+
+import org.apache.flink.client.program.ClusterClient;
+import org.apache.flink.client.program.JobWithJars;
+import org.apache.flink.client.program.ProgramInvocationException;
+import org.apache.flink.configuration.Configuration;
+import org.apache.flink.configuration.CoreOptions;
+import org.apache.flink.configuration.GlobalConfiguration;
+import org.apache.flink.configuration.TaskManagerOptions;
+import org.apache.flink.core.fs.Path;
+import org.apache.flink.runtime.client.JobExecutionException;
+import org.apache.flink.runtime.jobgraph.JobGraph;
+import org.apache.flink.runtime.minicluster.FlinkMiniCluster;
+import org.apache.flink.runtime.minicluster.LocalFlinkMiniCluster;
+import org.apache.flink.streaming.api.graph.StreamGraph;
+
+import com.bytedance.flink.configuration.Constants;
+import com.bytedance.flink.parser.ConfigParser;
+import com.bytedance.flink.pojo.JobConfig;
+import com.bytedance.flink.pojo.RuntimeConfig;
+import com.bytedance.flink.utils.EnvironmentInitUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URL;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Flink submitter, the main entrance class.
+ */
+public class FlinkSubmitter {
+	private static final Logger LOG = LoggerFactory.getLogger(FlinkSubmitter.class);
+	private String flinkConfDir;
+	private Configuration flinkConfig;
+	private String userJar;
+	private String yamlConf;
+	private String clusterName;
+	private boolean isDetached;
+	/**
+	 * The Flink mini cluster on which to execute the programs.
+	 */
+	private FlinkMiniCluster flinkMiniCluster;
+	private FlinkTopologyBuilder flinkTopologyBuilder;
+
+	public static void main(String[] args) throws Exception {
+		String[] newArgs = new String[args.length];
+		for (int i = 0; i < args.length; i++) {
+			newArgs[i] = args[i].replace('#', ' ');
+		}
+		FlinkSubmitter flinkSubmitter = new FlinkSubmitter();
+		flinkSubmitter.submit(newArgs);
+	}
+
+	public void submit(String[] args) throws Exception {
+		parseConfig(args);
+		JobConfig jobConfig = flinkTopologyBuilder.getJobConfig();
+		RuntimeConfig runtimeConfig = parseRuntimeConfig(jobConfig);
+		flinkTopologyBuilder.getEnv().getConfig().setGlobalJobParameters(runtimeConfig);
+		if (jobConfig.getRunMode() == 0) {
+			LOG.info("Run job on cluster mode");
+			jobConfig.setDetached(isDetached);
+			submitClusterJob();
+		} else {
+			LOG.info("Run job on local mode");
+			jobConfig.setDetached(false);
+			submitLocalJob();
+			int runTimeMs = jobConfig.getRunSeconds() * 1000;
+			LOG.info("The local cluster will shut down after {} seconds.", jobConfig.getRunSeconds());
+			Thread.sleep(runTimeMs);
+			if (flinkMiniCluster != null) {
+				flinkMiniCluster.stop();
+				flinkMiniCluster = null;
+			}
+		}
+	}
+
+	private void parseConfig(String[] args) throws Exception {
+		// 1.load system property
+		loadSystemProperty();
+		// 2.load configuration in flink-conf.yaml
+		flinkConfig = GlobalConfiguration.loadConfiguration();
+		// 3.load job configuration in user yaml
+		ConfigParser configParser = new ConfigParser(yamlConf);
+		JobConfig jobConfig = configParser.parse();
+
+		LOG.info("jobConfig = {}", jobConfig);
+		flinkTopologyBuilder = new FlinkTopologyBuilder(jobConfig);
+		String[] flinkYarnArgs = Arrays.copyOfRange(args, 0, args.length);
+		flinkTopologyBuilder.setFlinkYarnArgs(flinkYarnArgs);
+		flinkTopologyBuilder.build();
+	}
+
+	private void loadSystemProperty() {
+		flinkConfDir = System.getProperty(Constants.FLINK_CONF_DIR_PROPERTY);
+		assert flinkConfDir != null;
+		yamlConf = System.getProperty(Constants.YAML_CONF_PROPERTY);
+		assert yamlConf != null;
+		clusterName = System.getProperty(Constants.CLUSTER_NAME);
+		assert yamlConf != null;
+
+		userJar = System.getProperty(Constants.USER_JAR);
+		assert userJar != null;
+		isDetached = Boolean.valueOf(System.getProperty(Constants.DETACHED_PROPERTY, "true"));
+	}
+
+	private void submitClusterJob() {
+		final URI uploadedJarUri;
+		final URL uploadedJarUrl;
+		try {
+			uploadedJarUri = new File(userJar).getAbsoluteFile().toURI();
+			uploadedJarUrl = uploadedJarUri.toURL();
+			JobWithJars.checkJarFile(uploadedJarUrl);
+		} catch (final IOException e) {
+			throw new RuntimeException("Problem with jar file " + userJar, e);
+		}
+
+		StreamGraph streamGraph = flinkTopologyBuilder.getEnv().getStreamGraph();
+		streamGraph.setJobName(flinkTopologyBuilder.getJobName());
+
+		JobGraph jobGraph = streamGraph.getJobGraph();
+		jobGraph.addJar(new Path(uploadedJarUri));
+
+		ClusterClient client;
+		try {
+			client = getClusterClient();
+		} catch (Exception e) {
+			throw new RuntimeException("Could not establish a connection to the job manager", e);
+		}
+		try {
+			ClassLoader classLoader = JobWithJars.buildUserCodeClassLoader(
+				Collections.singletonList(uploadedJarUrl),
+				Collections.emptyList(),
+				this.getClass().getClassLoader());
+			boolean isDetached = flinkTopologyBuilder.getJobConfig().isDetached();
+			if (isDetached) {
+				client.runDetached(jobGraph, classLoader);
+			} else {
+				client.run(jobGraph, classLoader);
+			}
+		} catch (final ProgramInvocationException e) {
+			throw new RuntimeException("Cannot execute job due to ProgramInvocationException", e);
+		}
+	}
+
+	private void submitLocalJob() throws JobExecutionException {
+		StreamGraph streamGraph = flinkTopologyBuilder.getEnv().getStreamGraph();
+		streamGraph.setJobName(flinkTopologyBuilder.getJobName());
+
+		JobGraph jobGraph = streamGraph.getJobGraph();
+		if (flinkMiniCluster == null) {
+			Configuration configuration = new Configuration();
+			configuration.addAll(jobGraph.getJobConfiguration());
+
+			configuration.setLong(TaskManagerOptions.MANAGED_MEMORY_SIZE, -1L);
+			configuration.setInteger(TaskManagerOptions.NUM_TASK_SLOTS, jobGraph.getMaximumParallelism());
+
+			this.flinkMiniCluster = new LocalFlinkMiniCluster(configuration, true);
+			this.flinkMiniCluster.start();
+			this.flinkMiniCluster.submitJobAndWait(jobGraph, false);
+		}
+	}
+
+	private ClusterClient getClusterClient() throws Exception {
+		YarnCommandLine yarnCommandLine = new YarnCommandLine(flinkConfDir);
+		return yarnCommandLine.createClusterClient(flinkTopologyBuilder.getFlinkYarnArgs());
+	}
+
+	private RuntimeConfig parseRuntimeConfig(JobConfig jobConfig) {
+		RuntimeConfig runtimeConfig = new RuntimeConfig();
+		runtimeConfig.setJobName(jobConfig.getJobName());
+		runtimeConfig.setOwners(jobConfig.getOwners());
+		runtimeConfig.setClusterName(clusterName);
+		runtimeConfig.setIgnoreMismatchedMsg(jobConfig.isIgnoreMismatchedMsg());
+
+		// Infer that we are in new mode, user can access through 'conf' in callee
+		runtimeConfig.put(Constants.USE_NEW_MODE, true);
+		List<String> resources = EnvironmentInitUtils.getResourceFileList(userJar);
+		if (resources == null || resources.isEmpty()) {
+			throw new RuntimeException("Resource list cannot be null or empty.");
+		}
+		runtimeConfig.setResourceFiles(EnvironmentInitUtils.getResourceFileList(userJar));
+
+		String tempDir;
+		if (jobConfig.getRunMode() == 0) {
+			// cluster mode
+			tempDir = Constants.TEMP_DIR_VALUE;
+		} else {
+			// local mode
+			tempDir = flinkConfig.getString(CoreOptions.TMP_DIRS, "");
+		}
+		runtimeConfig.setCodeDir(Paths.get(tempDir, Constants.CODE_DIR_VALUE).toString());
+		runtimeConfig.setPidDir(Constants.PID_DIR_VALUE);
+		runtimeConfig.setEnvironment(jobConfig.getEnvironment());
+		runtimeConfig.put(Constants.TOPOLOGY_YAML, jobConfig.getTopologyYaml());
+		runtimeConfig.put(Constants.LOCAL_FAILOVER, jobConfig.isLocalFailover());
+		return runtimeConfig;
+	}
+}
---
+++ flink-contrib/flink-python-byted/src/main/java/com/bytedance/flink/topology/FlinkTopologyBuilder.java
@@ -0,0 +1,472 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.bytedance.flink.topology;
+
+import org.apache.flink.api.common.typeinfo.TypeInformation;
+import org.apache.flink.api.java.tuple.Tuple;
+import org.apache.flink.api.java.typeutils.TypeExtractor;
+import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.api.datastream.DataStreamSource;
+import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.functions.co.CoFlatMapFunction;
+import org.apache.flink.streaming.api.functions.sink.SinkFunction;
+import org.apache.flink.util.Collector;
+
+import com.bytedance.flink.component.BatchBoltProcess;
+import com.bytedance.flink.component.Bolt;
+import com.bytedance.flink.component.BoltWrapper;
+import com.bytedance.flink.component.ShellBolt;
+import com.bytedance.flink.component.ShellSpout;
+import com.bytedance.flink.component.SpoutWrapper;
+import com.bytedance.flink.configuration.Constants;
+import com.bytedance.flink.pojo.BoltConfig;
+import com.bytedance.flink.pojo.BoltInfo;
+import com.bytedance.flink.pojo.JobConfig;
+import com.bytedance.flink.pojo.Schema;
+import com.bytedance.flink.pojo.SpoutConfig;
+import com.bytedance.flink.pojo.SpoutInfo;
+import com.bytedance.flink.utils.CommonUtils;
+import com.bytedance.flink.utils.Validation;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * Flink topology builder.
+ */
+public class FlinkTopologyBuilder {
+	private static final Logger LOG = LoggerFactory.getLogger(FlinkTopologyBuilder.class);
+	private final HashMap<String, List<Grouping>> unprocessdInputsPerBolt =
+		new HashMap<>();
+	private HashMap<String, DataStream<Tuple>> availableInputs = new HashMap<>();
+	private Set<String> usedSpouts = new HashSet<>();
+
+	private JobConfig jobConfig;
+	private String jobName;
+	private StreamExecutionEnvironment env;
+	private String[] flinkYarnArgs;
+
+	public FlinkTopologyBuilder(JobConfig jobConfig) {
+		this.jobConfig = jobConfig;
+		this.jobName = jobConfig.getJobName();
+		this.env = StreamExecutionEnvironment.getExecutionEnvironment();
+		// Set default parallelism to 1.
+		env.setParallelism(1);
+	}
+
+	public void build() {
+		addSpouts();
+		addBolts();
+		addFakeBoltForSingleSpout();
+	}
+
+	public void addSpouts() {
+		SpoutConfig spoutConfig = jobConfig.getSpoutConfig();
+		for (Map.Entry<String, SpoutInfo> entry : spoutConfig.getSpoutInfoMap().entrySet()) {
+			String spoutName = entry.getKey();
+			SpoutInfo spoutInfo = entry.getValue();
+			List<String> outputFileds = spoutInfo.getOutputFields();
+			int numOfOutputFileds = outputFileds.size();
+
+			SpoutWrapper<Tuple> spoutWrapper =
+				new SpoutWrapper<>(new ShellSpout(spoutInfo), spoutName, numOfOutputFileds);
+
+			DataStreamSource<Tuple> source =
+				env.addSource(spoutWrapper, spoutName, getOutputType(outputFileds))
+					.setParallelism(spoutInfo.getParallelism());
+			availableInputs.put(spoutName, source);
+		}
+	}
+
+	public void addBolts() {
+		BoltConfig boltConfig = jobConfig.getBoltConfig();
+		Map<String, BoltInfo> boltInfoMap = new HashMap<>(boltConfig.getBoltInfoMap());
+
+		boolean makeProgress = true;
+		while (boltInfoMap.size() > 0) {
+			Iterator<Map.Entry<String, BoltInfo>> boltsIterator = boltInfoMap.entrySet().iterator();
+			if (!makeProgress) {
+				reportNoProgressException(boltInfoMap);
+			}
+			makeProgress = false;
+			while (boltsIterator.hasNext()) {
+				Map.Entry<String, BoltInfo> entry = boltsIterator.next();
+				String boltName = entry.getKey();
+				BoltInfo boltInfo = entry.getValue();
+				List<Grouping> groupingList = unprocessdInputsPerBolt.get(boltName);
+				if (groupingList == null) {
+					groupingList = boltInfo.getGroupList();
+					unprocessdInputsPerBolt.put(boltName, groupingList);
+				}
+				// Number of input Streams of this bolt.
+				int numberOfInputs = groupingList.size();
+				int inputsAvailable = 0;
+				for (Grouping grouping : groupingList) {
+					if (availableInputs.containsKey(grouping.getInputStream())) {
+						inputsAvailable++;
+					}
+				}
+
+				if (inputsAvailable != numberOfInputs) {
+					// traverse other bolts first until inputs are available
+					continue;
+				} else {
+					makeProgress = true;
+					boltsIterator.remove();
+				}
+
+				List<DataStream<Tuple>> inputStreams = new ArrayList<>(numberOfInputs);
+				for (Grouping grouping : groupingList) {
+					String inputStreamName = grouping.getInputStream();
+					DataStream<Tuple> inputStream = availableInputs.get(inputStreamName);
+					usedSpouts.add(inputStreamName);
+					DataStream<Tuple> processedInputStream = processInput(boltName, new ShellBolt(boltInfo),
+						grouping, inputStreamName, inputStream);
+					inputStreams.add(processedInputStream);
+				}
+
+				int parallelism = boltInfo.getParallelism();
+				SingleOutputStreamOperator<?> outputStream =
+					process(boltName, new ShellBolt(boltInfo), inputStreams).setParallelism(parallelism);
+			}
+		}
+	}
+
+	private void addFakeBoltForSingleSpout() {
+		SpoutConfig spoutConfig = jobConfig.getSpoutConfig();
+		Map<String, SpoutInfo> spoutInfoMap = spoutConfig.getSpoutInfoMap();
+		availableInputs.forEach((spoutId, stream) -> {
+			if (!spoutInfoMap.containsKey(spoutId)) {
+				return;
+			}
+			if (usedSpouts.contains(spoutId)) {
+				return;
+			}
+			stream.forward().addSink(new SinkFunction<Tuple>() {
+				@Override
+				public void invoke(Tuple value, Context context) throws Exception {
+					throw new RuntimeException("This is a faked bolt, can't access here.");
+				}
+			}).name(spoutId + "-FakerBolt").setParallelism(spoutInfoMap.get(spoutId).getParallelism());
+		});
+	}
+
+	private DataStream<Tuple> processInput(String boltName, Bolt bolt, Grouping grouping,
+		String inputStreamName, DataStream<Tuple> inputStream) {
+		assert boltName != null;
+		assert bolt != null;
+		assert grouping != null;
+		assert inputStreamName != null;
+		assert inputStream != null;
+
+		GroupType groupType = grouping.getGroupType();
+		switch (groupType) {
+			case SHUFFLE:
+				// We use rebalance for 'SHFFULE' in old version, while we change it to real shuffle now.
+				inputStream = inputStream.shuffle();
+				break;
+			case FORWARD:
+				inputStream = inputStream.forward();
+				break;
+			case RESCALE:
+				inputStream = inputStream.rescale();
+				break;
+			case REBALANCE:
+				inputStream = inputStream.rebalance();
+				break;
+			case BROADCAST:
+				inputStream = inputStream.broadcast();
+				break;
+			case KEY_BY:
+				List<String> groupFields = grouping.getGroupFields();
+				if (groupFields == null || groupFields.isEmpty()) {
+					throw new RuntimeException("No grouping fields specified.");
+				}
+				int[] keyByIndexes = getGroupingFieldIndexes(inputStreamName, grouping.getGroupFields());
+				inputStream = inputStream.keyBy(keyByIndexes);
+				break;
+			default:
+				throw new UnsupportedOperationException(
+					String.format("Unsupported grouping type: %s", groupType));
+		}
+		return inputStream;
+
+	}
+
+	private SingleOutputStreamOperator<?> process(String boltName, Bolt bolt,
+		List<DataStream<Tuple>> inputStreams) {
+		assert (boltName != null);
+		assert (bolt != null);
+		assert (inputStreams != null) && (!inputStreams.isEmpty());
+
+		BoltInfo boltInfo = bolt.getBoltInfo();
+		int parallelism = boltInfo.getParallelism();
+		Schema outputSchema = boltInfo.getOutputSchema();
+
+		Iterator<DataStream<Tuple>> iterator = inputStreams.iterator();
+		DataStream<Tuple> dataStream = null;
+		DataStream<Tuple>[] restStreams = new DataStream[inputStreams.size() - 1];
+
+		if (isInputSameFields(boltInfo)) {
+			// If all input streams have the same output fields size, we use 'union'.
+			int index = 0;
+			while (iterator.hasNext()) {
+				if (dataStream == null) {
+					dataStream = iterator.next();
+				} else {
+					restStreams[index] = iterator.next();
+					index++;
+				}
+			}
+			if (restStreams.length >= 1) {
+				assert dataStream != null;
+				dataStream = dataStream.union(restStreams);
+			}
+		} else {
+			int index = 0;
+			while (iterator.hasNext()) {
+				DataStream<Tuple> tempDataStream = iterator.next();
+				if (dataStream == null) {
+					dataStream = tempDataStream;
+				} else {
+					int maxInputFileds = getMaxInputFileds(boltInfo);
+					dataStream = dataStream.connect(tempDataStream)
+						.flatMap(new CoFlatMapFunction<Tuple, Tuple, Tuple>() {
+							@Override
+							public void flatMap1(Tuple value, Collector<Tuple> out) throws Exception {
+								out.collect(CommonUtils.extendTuple(value, maxInputFileds));
+							}
+
+							@Override
+							public void flatMap2(Tuple value, Collector<Tuple> out) throws Exception {
+								out.collect(CommonUtils.extendTuple(value, maxInputFileds));
+							}
+						}).name("co-flat-" + boltName + "-" + index)
+						.returns(getConnectedInputsTypeInfo(boltInfo))
+						.setParallelism(boltInfo.getParallelism());
+					index++;
+				}
+			}
+		}
+
+		if (isBatchBolt(boltInfo)) {
+			List<Grouping> groupingList = boltInfo.getGroupList();
+			Validation.validateNotEmptyList(groupingList, "groupingList");
+			Grouping grouping = groupingList.get(0);
+			LOG.info("GroupType = {}", grouping.getGroupType());
+			LOG.info("grouping = {}", grouping);
+			if (!grouping.getGroupType().equals(GroupType.KEY_BY)) {
+				throw new RuntimeException("The grouping type before batch bolt must be keyby('fields'|'key_by')");
+			}
+			long batchIntervalMs = (int) boltInfo.getArgs().getOrDefault(Constants.FLUSH_INTERVAL_SECONDS,
+				Constants.FLUSH_INTERVAL_SECONDS_DEFAULT) * 1000;
+			Schema inputSchema = jobConfig.getBoltConfig().getBoltInfoMap().get(grouping.getInputStream()).getOutputSchema();
+			BatchBoltProcess<Tuple, Object> process = new BatchBoltProcess<>(new ShellBolt(boltInfo),
+				boltName, outputSchema, batchIntervalMs,
+				getOutputType(inputSchema.toList()));
+			assert dataStream != null;
+			int[] keyByIndexes = getGroupingFieldIndexes(grouping.getInputStream(), grouping.getGroupFields());
+
+			return dataStream.keyBy(keyByIndexes)
+				.process(process).setParallelism(parallelism).name(boltName)
+				.returns(Object.class);
+		}
+
+		SingleOutputStreamOperator<Tuple> outputStream;
+
+		TypeInformation<Tuple> outType = getOutputType(outputSchema.toList());
+
+		BoltWrapper<Tuple, Tuple> boltWrapper = new BoltWrapper<>(new ShellBolt(boltInfo), boltName,
+			outputSchema);
+		assert dataStream != null;
+		outputStream = dataStream.transform(boltName, outType, boltWrapper);
+		if (outType != null) {
+			availableInputs.put(boltName, outputStream);
+		}
+		return outputStream;
+	}
+
+	private int getMaxInputFileds(BoltInfo boltInfo) throws IllegalArgumentException {
+		List<Grouping> groupList = boltInfo.getGroupList();
+		int maxFieldSize = -1;
+		for (Grouping grouping : groupList) {
+			int fieldSizeTemp;
+			String inputStreamName = grouping.getInputStream();
+			Map<String, SpoutInfo> spoutMap = jobConfig.getSpoutConfig().getSpoutInfoMap();
+			Map<String, BoltInfo> boltMap = jobConfig.getBoltConfig().getBoltInfoMap();
+			if (spoutMap.containsKey(inputStreamName)) {
+				fieldSizeTemp = spoutMap.get(inputStreamName).getOutputFields().size();
+			} else {
+				fieldSizeTemp = boltMap.get(inputStreamName).getOutputFields().size();
+			}
+			maxFieldSize = Math.max(maxFieldSize, fieldSizeTemp);
+		}
+		return maxFieldSize;
+	}
+
+	private TypeInformation<Tuple> getConnectedInputsTypeInfo(BoltInfo boltInfo) throws IllegalArgumentException {
+		int maxFieldSize = getMaxInputFileds(boltInfo);
+		return getOutputType(maxFieldSize);
+	}
+
+	private TypeInformation<Tuple> getOutputType(int numberOfAttributes) throws IllegalArgumentException {
+		if (numberOfAttributes < 0) {
+			throw new RuntimeException("numberOfAttributes must be greater than or equal to 0.");
+		}
+
+		Tuple t;
+		if (numberOfAttributes <= 25) {
+			try {
+				t = Tuple.getTupleClass(numberOfAttributes).newInstance();
+			} catch (final InstantiationException e) {
+				throw new RuntimeException(e);
+			} catch (final IllegalAccessException e) {
+				throw new RuntimeException(e);
+			}
+		} else {
+			throw new IllegalArgumentException("Flink supports only a maximum number of 24 attributes");
+		}
+
+		for (int i = 0; i < numberOfAttributes; ++i) {
+			t.setField(new DefaultComparator(), i);
+		}
+
+		return TypeExtractor.getForObject(t);
+	}
+
+	private TypeInformation<Tuple> getOutputType(List<String> outputFileds) throws IllegalArgumentException {
+		if (outputFileds == null) {
+			throw new RuntimeException("OutputFileds must not be null.");
+		}
+
+		Tuple t;
+		final int numberOfAttributes = outputFileds.size();
+
+		return getOutputType(numberOfAttributes);
+	}
+
+	/**
+	 * Computes the indexes within the declared output schema of the specified stream, for a list of given
+	 * field-grouping attributes.
+	 *
+	 * @param groupingFields The names of the key fields.
+	 * @return array of {@code int}s that contains the index within the output schema for each attribute in the given
+	 * list
+	 */
+	private int[] getGroupingFieldIndexes(String inputStreamName, final List<String> groupingFields) {
+		LOG.info("inputStreamName = " + inputStreamName);
+		LOG.info("groupingFields = " + groupingFields);
+		final int[] fieldIndexes = new int[groupingFields.size()];
+		for (int i = 0; i < fieldIndexes.length; ++i) {
+			Schema outputSchema;
+			if (jobConfig.getSpoutConfig().getSpoutInfoMap().containsKey(inputStreamName)) {
+				SpoutInfo spoutInfo = jobConfig.getSpoutConfig().getSpoutInfoMap().get(inputStreamName);
+				outputSchema = spoutInfo.getOutputSchema();
+			} else {
+				BoltInfo boltInfo = jobConfig.getBoltConfig().getBoltInfoMap().get(inputStreamName);
+				outputSchema = boltInfo.getOutputSchema();
+			}
+			fieldIndexes[i] = outputSchema.fieldIndex(groupingFields.get(i));
+		}
+		return fieldIndexes;
+	}
+
+	private boolean isBatchBolt(BoltInfo boltInfo) {
+		return boltInfo.getScript().contains(Constants.BATCH_BOLT);
+	}
+
+	private boolean isInputSameFields(BoltInfo boltInfo) {
+		List<Grouping> groupList = boltInfo.getGroupList();
+		int fieldSize = -1;
+		for (Grouping grouping : groupList) {
+			int fieldSizeTemp;
+			String inputStreamName = grouping.getInputStream();
+			Map<String, SpoutInfo> spoutMap = jobConfig.getSpoutConfig().getSpoutInfoMap();
+			Map<String, BoltInfo> boltMap = jobConfig.getBoltConfig().getBoltInfoMap();
+			if (spoutMap.containsKey(inputStreamName)) {
+				fieldSizeTemp = spoutMap.get(inputStreamName).getOutputFields().size();
+			} else {
+				fieldSizeTemp = boltMap.get(inputStreamName).getOutputFields().size();
+			}
+			if (fieldSize < 0) {
+				fieldSize = fieldSizeTemp;
+			} else if (fieldSize != fieldSizeTemp) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+	private void reportNoProgressException(Map<String, BoltInfo> boltInfoMap) {
+		StringBuilder strBld = new StringBuilder();
+		strBld.append("Unable to build Topology. Could not connect the following bolts:");
+		for (String boltId : boltInfoMap.keySet()) {
+			strBld.append("\n  ");
+			strBld.append(boltId);
+			strBld.append(": missing input streams [");
+			for (Grouping grouping : unprocessdInputsPerBolt
+				.get(boltId)) {
+				strBld.append("'");
+				strBld.append(grouping.getInputStream());
+				strBld.append("'; ");
+			}
+			strBld.append("]");
+		}
+		throw new RuntimeException(strBld.toString());
+	}
+
+	public JobConfig getJobConfig() {
+		return jobConfig;
+	}
+
+	public void setJobConfig(JobConfig jobConfig) {
+		this.jobConfig = jobConfig;
+	}
+
+	public StreamExecutionEnvironment getEnv() {
+		return env;
+	}
+
+	public void setEnv(StreamExecutionEnvironment env) {
+		this.env = env;
+	}
+
+	public String getJobName() {
+		return jobName;
+	}
+
+	public void setJobName(String jobName) {
+		this.jobName = jobName;
+	}
+
+	public String[] getFlinkYarnArgs() {
+		return flinkYarnArgs;
+	}
+
+	public void setFlinkYarnArgs(String[] flinkYarnArgs) {
+		this.flinkYarnArgs = flinkYarnArgs;
+	}
+}
