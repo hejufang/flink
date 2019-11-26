55570ba3beeda99914be2d5674a23416a5d634d0
zoudan <zoudan@bytedance.com>
2019-11-26 16:29:46 +0800

    [INFOI-5336] add connectors & formats builder

---
+++ build_connectors.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+################################################################################
+#  Licensed to the Apache Software Foundation (ASF) under one
+#  or more contributor license agreements.  See the NOTICE file
+#  distributed with this work for additional information
+#  regarding copyright ownership.  The ASF licenses this file
+#  to you under the Apache License, Version 2.0 (the
+#  "License"); you may not use this file except in compliance
+#  with the License.  You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+# limitations under the License.
+################################################################################
+# build connector libraries.
+rm -rf output/runtime_files
+mkdir -p output/runtime_files
+cd flink-connectors
+mvn clean package -DskipTests -T 1C
+
+cp flink-connector-clickhouse/target/flink-connector-clickhouse-1.9-byted-SNAPSHOT.jar ../output/runtime_files
+cp flink-connector-redis/target/flink-connector-redis-1.9-byted-SNAPSHOT.jar ../output/runtime_files
+cp flink-jdbc/target/flink-jdbc_2.11-1.9-byted-SNAPSHOT.jar ../output/runtime_files
+cp flink-connector-kafka-base/target/flink-connector-kafka-base_2.11-1.9-byted-SNAPSHOT.jar ../output/runtime_files
+cp flink-connector-kafka-0.9/target/flink-connector-kafka-0.9_2.11-1.9-byted-SNAPSHOT.jar ../output/runtime_files
+cp flink-connector-kafka-0.10/target/flink-connector-kafka-0.10_2.11-1.9-byted-SNAPSHOT.jar ../output/runtime_files
+cp flink-connector-redis/target/flink-connector-redis-1.9-byted-SNAPSHOT.jar ../output/runtime_files
+cp flink-connector-print/target/flink-connector-print-1.9-byted-SNAPSHOT.jar ../output/runtime_files
