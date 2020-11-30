622d83907a6606ba688679a5cb059bb363923440
zhangyunfan <yunfanfighting@foxmail.com>
2020-11-30 11:16:44 +0000

    [INFOI-15900] Support binlog format.

---
+++ flink-formats/flink-binlog/src/main/java/org/apache/flink/formats/binlog/BinlogRowDeserializationSchema.java
@@ -0,0 +1,279 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.flink.formats.binlog;
+
+import org.apache.flink.api.common.serialization.DeserializationSchema;
+import org.apache.flink.api.common.typeinfo.TypeInformation;
+import org.apache.flink.formats.pb.DeserializationRuntimeConverterFactory;
+import org.apache.flink.table.api.ValidationException;
+import org.apache.flink.table.data.GenericRowData;
+import org.apache.flink.table.data.RowData;
+import org.apache.flink.table.types.logical.BooleanType;
+import org.apache.flink.table.types.logical.IntType;
+import org.apache.flink.table.types.logical.LogicalType;
+import org.apache.flink.table.types.logical.RowType;
+import org.apache.flink.table.types.logical.VarCharType;
+import org.apache.flink.util.FlinkRuntimeException;
+
+import com.bytedance.binlog.DRCEntry;
+import com.google.protobuf.ByteString;
+import com.google.protobuf.DynamicMessage;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.util.Base64;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.regex.Pattern;
+
+import static org.apache.flink.formats.binlog.BinlogOptions.AFTER_PREFIX;
+import static org.apache.flink.formats.binlog.BinlogOptions.BEFORE_PREFIX;
+import static org.apache.flink.formats.binlog.BinlogOptions.BINLOG_BODY;
+import static org.apache.flink.formats.binlog.BinlogOptions.BINLOG_HEADER;
+import static org.apache.flink.formats.binlog.BinlogOptions.BODY;
+import static org.apache.flink.formats.binlog.BinlogOptions.ENTRY;
+import static org.apache.flink.formats.binlog.BinlogOptions.HEADER;
+import static org.apache.flink.formats.binlog.BinlogOptions.INDEX_COLUMN;
+import static org.apache.flink.formats.binlog.BinlogOptions.IS_NULLABLE_COLUMN;
+import static org.apache.flink.formats.binlog.BinlogOptions.IS_PK_COLUMN;
+import static org.apache.flink.formats.binlog.BinlogOptions.IS_UNSIGNED_COLUMN;
+import static org.apache.flink.formats.binlog.BinlogOptions.MESSAGE;
+import static org.apache.flink.formats.binlog.BinlogOptions.NAME_COLUMN;
+import static org.apache.flink.formats.binlog.BinlogOptions.NULL_COLUMN;
+import static org.apache.flink.formats.binlog.BinlogOptions.PAYLOAD;
+import static org.apache.flink.formats.binlog.BinlogOptions.ROWDATAS;
+import static org.apache.flink.formats.binlog.BinlogOptions.SQL_TYPE_COLUMN;
+import static org.apache.flink.formats.binlog.BinlogOptions.TABLE;
+import static org.apache.flink.formats.binlog.BinlogOptions.UPDATED_COLUMN;
+import static org.apache.flink.formats.binlog.BinlogOptions.VALUE_COLUMN;
+import static org.apache.flink.formats.binlog.RuntimeConverterFactory.DESCRIPTORS;
+import static org.apache.flink.formats.binlog.RuntimeConverterFactory.FIELD_DESCRIPTORS;
+import static org.apache.flink.formats.pb.DeserializationRuntimeConverterFactory.DeserializationRuntimeConverter;
+
+/**
+ * Binlog row deserialization schema.
+ */
+public class BinlogRowDeserializationSchema implements DeserializationSchema<RowData> {
+	private static final Logger LOG = LoggerFactory.getLogger(BinlogRowDeserializationSchema.class);
+
+	private static final Map<String, Class<? extends LogicalType>> FIXED_TYPE_INFO = initFixedTypeInfo();
+	private final Map<String, RuntimeConverterFactory.RuntimeConverter> runtimeConverterMap;
+	private final DeserializationRuntimeConverter headerRuntimeConverter;
+	private final DeserializationRuntimeConverter bodyRuntimeConverter;
+
+	private final RowType rowType;
+	private final TypeInformation<RowData> resultTypeInfo;
+	private final boolean ignoreParseErrors;
+	private final Pattern targetTablePattern;
+
+	public BinlogRowDeserializationSchema(
+			RowType rowType,
+			TypeInformation<RowData> resultTypeInfo,
+			String targetTable,
+			boolean ignoreParseErrors) {
+		this.rowType = rowType;
+		this.resultTypeInfo = resultTypeInfo;
+		this.ignoreParseErrors = ignoreParseErrors;
+		this.targetTablePattern = Pattern.compile(targetTable);
+		this.headerRuntimeConverter = DeserializationRuntimeConverterFactory.createRowConverter(
+			(RowType) this.rowType.getTypeAt(this.rowType.getFieldIndex(BINLOG_HEADER)), DRCEntry.EntryHeader.getDescriptor());
+		this.bodyRuntimeConverter = DeserializationRuntimeConverterFactory.createRowConverter(
+			(RowType) this.rowType.getTypeAt(this.rowType.getFieldIndex(BINLOG_BODY)), DRCEntry.EntryBody.getDescriptor());
+		this.runtimeConverterMap = RuntimeConverterFactory.createConverter(this.rowType);
+		checkTypes();
+	}
+
+	@Override
+	public RowData deserialize(byte[] message) throws IOException {
+		try {
+			DynamicMessage binlogMessage = DynamicMessage.parseFrom(DESCRIPTORS.get(MESSAGE), message);
+			DynamicMessage entryMessage =
+				DynamicMessage.parseFrom(DESCRIPTORS.get(ENTRY),
+					(ByteString) binlogMessage.getField(FIELD_DESCRIPTORS.get(PAYLOAD)));
+			List<String> fieldNames = rowType.getFieldNames();
+			GenericRowData rowData = new GenericRowData(rowType.getFieldCount());
+			// 1. deserialize header
+			DynamicMessage header = (DynamicMessage) entryMessage.getField(FIELD_DESCRIPTORS.get(HEADER));
+
+			// filter the target data.
+			String tableName = (String) header.getField(FIELD_DESCRIPTORS.get(TABLE));
+			if (!targetTablePattern.matcher(tableName).matches()) {
+				// return null if the data are not from the target table.
+				return null;
+			}
+
+			Object headerRow =
+				headerRuntimeConverter.convert(header);
+			rowData.setField(0, headerRow);
+
+			// 2. deserialize body
+			DynamicMessage body = (DynamicMessage) entryMessage.getField(FIELD_DESCRIPTORS.get(BODY));
+			Object bodyRow =
+				bodyRuntimeConverter.convert(body);
+			rowData.setField(1, bodyRow);
+
+			// Fill user defind fields.
+			for (int i = 2; i < rowData.getArity(); i++) {
+				String fieldName = fieldNames.get(i);
+				// 3. deserialize rowdata
+				List<DynamicMessage> rowDataList =
+					(List<DynamicMessage>) body.getField(FIELD_DESCRIPTORS.get(ROWDATAS));
+				// Although rowDataList is a list, but binlog ensure that it has and only has one element.
+				DynamicMessage dynamicMessage = (DynamicMessage) rowDataList.get(0);
+				RuntimeConverterFactory.RuntimeConverter converter =
+					runtimeConverterMap.get(fieldName);
+				rowData.setField(i, converter.convert(dynamicMessage));
+			}
+			return rowData;
+		} catch (Throwable t) {
+			String errorMsg = String.format("Deserialized failed base64 `%s`.", Base64.getEncoder().encodeToString(message));
+			if (ignoreParseErrors) {
+				LOG.warn(errorMsg, t);
+				return null;
+			}
+			throw new FlinkRuntimeException(errorMsg, t);
+		}
+	}
+
+	@Override
+	public boolean isEndOfStream(RowData nextElement) {
+		return false;
+	}
+
+	@Override
+	public TypeInformation<RowData> getProducedType() {
+		return resultTypeInfo;
+	}
+
+	private static Map<String, Class<? extends LogicalType>> initFixedTypeInfo() {
+		Map<String, Class<? extends LogicalType>> fixedTypeInfoMap = new HashMap<>();
+
+		fixedTypeInfoMap.put(INDEX_COLUMN, IntType.class);
+		fixedTypeInfoMap.put(NAME_COLUMN, VarCharType.class);
+		fixedTypeInfoMap.put(IS_PK_COLUMN, BooleanType.class);
+		fixedTypeInfoMap.put(UPDATED_COLUMN, BooleanType.class);
+		fixedTypeInfoMap.put(IS_NULLABLE_COLUMN, BooleanType.class);
+		fixedTypeInfoMap.put(NULL_COLUMN, BooleanType.class);
+		fixedTypeInfoMap.put(SQL_TYPE_COLUMN, VarCharType.class);
+		fixedTypeInfoMap.put(IS_UNSIGNED_COLUMN, BooleanType.class);
+		fixedTypeInfoMap.put(VALUE_COLUMN, VarCharType.class);
+
+		return fixedTypeInfoMap;
+	}
+
+	public static Builder builder() {
+		return new Builder();
+	}
+
+	/**
+	 * Ensure that:
+	 * 1. All types of user defined column are row types except time attribute columns.
+	 * 2. All inner column names must be in format: 'before_'/'after_' + value in VALID_FIELD.
+	 * 3. All inner column type must be matched with FIXED_TYPE_INFO.
+	 */
+	private void checkTypes() {
+		List<String> fieldNames = rowType.getFieldNames();
+		for (int i = 0; i < rowType.getFieldCount(); i++) {
+			String typeName = fieldNames.get(i);
+			if (BINLOG_HEADER.equals(typeName) || BINLOG_BODY.equals(typeName)) {
+				continue;
+			}
+			LogicalType typeInformation = rowType.getTypeAt(i);
+
+			if (!(typeInformation instanceof RowType)) {
+				throw new ValidationException(String.format("All user defined types have " +
+					"to be Row type. But type of %s is %s.", typeName, typeInformation));
+			}
+
+			RowType innerRowType = (RowType) typeInformation;
+			List<String> innerColumnNames = innerRowType.getFieldNames();
+			for (int j = 0; j < innerRowType.getFieldCount(); j++) {
+				String innerColumnName = innerColumnNames.get(j);
+				LogicalType innerColumnLogicalType = innerRowType.getTypeAt(j);
+				if (innerColumnName == null) {
+					throw new ValidationException("Filed names in row cannot be null.");
+				}
+
+				String realField;
+				if (innerColumnName.startsWith(BEFORE_PREFIX)) {
+					realField = innerColumnName.substring(BEFORE_PREFIX.length());
+				} else if (innerColumnName.startsWith(AFTER_PREFIX)) {
+					realField = innerColumnName.substring(AFTER_PREFIX.length());
+				} else {
+					throw new ValidationException(
+						String.format("Inner column name must be start with %s or %s, " +
+							"but column name is %s.", BEFORE_PREFIX, AFTER_PREFIX, innerColumnName));
+				}
+				Class<? extends LogicalType> expectedTypeInfo = FIXED_TYPE_INFO.get(realField);
+				if (expectedTypeInfo == null) {
+					throw new ValidationException(
+						String.format("Invalid field: %s. Valid fields must be " +
+								"%s/%s + value in list %s, e.g. 'before_value'.",
+							innerColumnName, BEFORE_PREFIX, AFTER_PREFIX, FIXED_TYPE_INFO.values()));
+				}
+
+				// We do not need to validate types for colume 'value' here.
+				if (!VALUE_COLUMN.equals(realField) && !expectedTypeInfo.isInstance(innerColumnLogicalType)) {
+					throw new ValidationException(
+						String.format("Expect flink type for column '%s' is %s, but we get '%s' " +
+							"parsed from DDL.", innerColumnName, expectedTypeInfo, innerColumnLogicalType));
+				}
+			}
+		}
+	}
+
+	/**
+	 * BinlogRowDeserializationSchema builder.
+	 */
+	public static class Builder {
+		private RowType rowType;
+		private TypeInformation<RowData> resultTypeInfo;
+		private String targetTable;
+		private boolean ignoreParseErrors;
+
+		private Builder() {
+		}
+
+		public Builder setRowType(RowType rowType) {
+			this.rowType = rowType;
+			return this;
+		}
+
+		public Builder setResultTypeInfo(TypeInformation<RowData> resultTypeInfo) {
+			this.resultTypeInfo = resultTypeInfo;
+			return this;
+		}
+
+		public Builder setTargetTable(String targetTable) {
+			this.targetTable = targetTable;
+			return this;
+		}
+
+		public Builder setIgnoreParseErrors(boolean ignoreParseErrors) {
+			this.ignoreParseErrors = ignoreParseErrors;
+			return this;
+		}
+
+		public BinlogRowDeserializationSchema build() {
+			return new BinlogRowDeserializationSchema(rowType, resultTypeInfo, targetTable, ignoreParseErrors);
+		}
+	}
+}
---
+++ flink-formats/flink-binlog/src/main/java/org/apache/flink/formats/binlog/RuntimeConverterFactory.java
@@ -0,0 +1,321 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.flink.formats.binlog;
+
+import org.apache.flink.table.data.GenericRowData;
+import org.apache.flink.table.types.logical.BigIntType;
+import org.apache.flink.table.types.logical.CharType;
+import org.apache.flink.table.types.logical.DateType;
+import org.apache.flink.table.types.logical.DoubleType;
+import org.apache.flink.table.types.logical.FloatType;
+import org.apache.flink.table.types.logical.IntType;
+import org.apache.flink.table.types.logical.LogicalType;
+import org.apache.flink.table.types.logical.RowType;
+import org.apache.flink.table.types.logical.SmallIntType;
+import org.apache.flink.table.types.logical.TimeType;
+import org.apache.flink.table.types.logical.TimestampType;
+import org.apache.flink.table.types.logical.TinyIntType;
+import org.apache.flink.table.types.logical.VarCharType;
+import org.apache.flink.util.FlinkRuntimeException;
+
+import com.bytedance.binlog.DRCEntry;
+import com.google.protobuf.Descriptors;
+import com.google.protobuf.DynamicMessage;
+
+import java.io.Serializable;
+import java.sql.Date;
+import java.sql.Time;
+import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static org.apache.flink.formats.binlog.BinlogOptions.AFTER_IMAGE;
+import static org.apache.flink.formats.binlog.BinlogOptions.AFTER_PREFIX;
+import static org.apache.flink.formats.binlog.BinlogOptions.BEFORE_IMAGE;
+import static org.apache.flink.formats.binlog.BinlogOptions.BEFORE_PREFIX;
+import static org.apache.flink.formats.binlog.BinlogOptions.BIGINT;
+import static org.apache.flink.formats.binlog.BinlogOptions.BODY;
+import static org.apache.flink.formats.binlog.BinlogOptions.CHAR;
+import static org.apache.flink.formats.binlog.BinlogOptions.DATE;
+import static org.apache.flink.formats.binlog.BinlogOptions.DATETIME;
+import static org.apache.flink.formats.binlog.BinlogOptions.DOUBLE;
+import static org.apache.flink.formats.binlog.BinlogOptions.ENTRY;
+import static org.apache.flink.formats.binlog.BinlogOptions.FLOAT;
+import static org.apache.flink.formats.binlog.BinlogOptions.HEADER;
+import static org.apache.flink.formats.binlog.BinlogOptions.INT;
+import static org.apache.flink.formats.binlog.BinlogOptions.MESSAGE;
+import static org.apache.flink.formats.binlog.BinlogOptions.NAME_COLUMN;
+import static org.apache.flink.formats.binlog.BinlogOptions.NULL_COLUMN;
+import static org.apache.flink.formats.binlog.BinlogOptions.PAYLOAD;
+import static org.apache.flink.formats.binlog.BinlogOptions.REAL;
+import static org.apache.flink.formats.binlog.BinlogOptions.ROWDATAS;
+import static org.apache.flink.formats.binlog.BinlogOptions.SMALLINT;
+import static org.apache.flink.formats.binlog.BinlogOptions.SQL_TYPE_COLUMN;
+import static org.apache.flink.formats.binlog.BinlogOptions.TABLE;
+import static org.apache.flink.formats.binlog.BinlogOptions.TEXT;
+import static org.apache.flink.formats.binlog.BinlogOptions.TIME;
+import static org.apache.flink.formats.binlog.BinlogOptions.TIMESTAMP;
+import static org.apache.flink.formats.binlog.BinlogOptions.TINYINT;
+import static org.apache.flink.formats.binlog.BinlogOptions.VALUE_COLUMN;
+import static org.apache.flink.formats.binlog.BinlogOptions.VARCHAR;
+
+/**
+ * Runtime convert factory.
+ */
+public class RuntimeConverterFactory {
+	public static final Map<String, Descriptors.FieldDescriptor> FIELD_DESCRIPTORS = initFieldDescriptors();
+	public static final Map<String, Descriptors.Descriptor> DESCRIPTORS = initDescriptors();
+	private static final Map<String, Class<? extends LogicalType>> JDBC_TO_FLINK_TYPE = getJdbcToFlinkType();
+	private static final String ZERO_TIMESTAMP_STR = "0000-00-00 00:00:00";
+
+	/**
+	 * Create runtime converter according to RowTypeInfo.
+	 */
+	public static Map<String, RuntimeConverter> createConverter(RowType rowType) {
+		Map<String, RuntimeConverter> converterMap = new HashMap<>();
+		List<String> columnNames = rowType.getFieldNames();
+
+		// First two field must be binlog_header & binlog_body, we ignore them. So we start from '2'.
+		for (int i = 2; i < rowType.getFieldCount(); i++) {
+			String columnName = columnNames.get(i);
+			LogicalType logicalType = rowType.getTypeAt(i);
+			String fieldName = columnNames.get(i);
+			if (!(logicalType instanceof RowType)) {
+				continue;
+			}
+
+			RowType innerRowType = (RowType) logicalType;
+			List<String> innerColumnNames = innerRowType.getFieldNames();
+			List<RuntimeConverter> innerConverterList = new ArrayList<>();
+			for (int j = 0; j < innerRowType.getFieldCount(); j++) {
+				String innerColumnName = innerColumnNames.get(j);
+				LogicalType innerLogicalType = innerRowType.getTypeAt(j);
+				String realColumnName = removeColumnNamePrefix(innerColumnName);
+				RuntimeConverter valueConverter =
+					createValueConverter(innerLogicalType, realColumnName);
+				innerConverterList.add(addConverterWrapper(valueConverter, innerLogicalType,
+					realColumnName, fieldName));
+			}
+
+			converterMap.put(columnName, assembleRowConverter(columnName, innerRowType, innerConverterList));
+		}
+		return converterMap;
+	}
+
+	private static RuntimeConverter assembleRowConverter(
+			String columnName,
+			RowType rowType,
+			List<RuntimeConverter> fieldConverters) {
+		return (msg) -> {
+			DynamicMessage rowData = (DynamicMessage) msg;
+			List<String> innerColumns = rowType.getFieldNames();
+			List<DynamicMessage> beforeImageList = (List) rowData.getField(FIELD_DESCRIPTORS.get(BEFORE_IMAGE));
+			List<DynamicMessage> afterImageList = (List) rowData.getField(FIELD_DESCRIPTORS.get(AFTER_IMAGE));
+
+			Map<String, DynamicMessage> beforeImageMap =
+				parseAllColumns(beforeImageList, FIELD_DESCRIPTORS.get(NAME_COLUMN));
+			Map<String, DynamicMessage> afterImageMap =
+				parseAllColumns(afterImageList, FIELD_DESCRIPTORS.get(NAME_COLUMN));
+			GenericRowData row = new GenericRowData(rowType.getFieldCount());
+			for (int i = 0; i < rowType.getFieldCount(); i++) {
+				String innerColumn = innerColumns.get(i);
+				DynamicMessage columnMessage;
+				if (innerColumn.startsWith(BEFORE_PREFIX)) {
+					columnMessage = beforeImageMap.get(columnName);
+				} else {
+					columnMessage = afterImageMap.get(columnName);
+				}
+				if (columnMessage == null) {
+					row.setField(i, null);
+				} else {
+					row.setField(i, fieldConverters.get(i).convert(columnMessage));
+				}
+			}
+			return row;
+		};
+	}
+
+	/**
+	 * We do the common work here when converter data, including:
+	 * 1. check mismatch for jdbc type & flink type.
+	 * 2. handle null data.
+	 */
+	private static RuntimeConverter addConverterWrapper(
+			RuntimeConverter converter,
+			LogicalType logicalType,
+			String innerColumnName,
+			String fieldName) {
+		return o -> {
+			DynamicMessage columnMessage = (DynamicMessage) o;
+
+			if (VALUE_COLUMN.equals(innerColumnName)) {
+				// validate type for column 'value'.
+				String jdbcTypeName = (String) columnMessage.getField(FIELD_DESCRIPTORS.get(SQL_TYPE_COLUMN));
+				validateType(jdbcTypeName, logicalType, fieldName, innerColumnName);
+				boolean isNull = (boolean) columnMessage.getField(FIELD_DESCRIPTORS.get(NULL_COLUMN));
+				if (isNull) {
+					// This indicates the column value is null.
+					return null;
+				}
+				String valueInString = (String) columnMessage.getField(FIELD_DESCRIPTORS.get(VALUE_COLUMN));
+				try {
+					return converter.convert(valueInString);
+				} catch (Throwable t) {
+					throw new FlinkRuntimeException(String.format("Failed to convert value '%s' to " +
+						"type '%s' for column '%s'.", valueInString, logicalType, fieldName), t);
+				}
+			}
+
+			return columnMessage.getField(DRCEntry.Column.getDescriptor().findFieldByName(innerColumnName));
+		};
+	}
+
+	private static void validateType(String jdbcTypeName, LogicalType logicalType, String fieldName, String innerName) {
+		Class<? extends LogicalType> expectedTypeInfo = JDBC_TO_FLINK_TYPE.get(jdbcTypeName);
+		if (expectedTypeInfo == null) {
+			throw new IllegalArgumentException(String.format("Unsupported jdbc type: %s.", jdbcTypeName));
+		}
+		if (!expectedTypeInfo.isInstance(logicalType)) {
+			throw new IllegalArgumentException(
+				String.format("Expect flink type for jdbc type '%s' is %s, but we get '%s' parsed from DDL, " +
+					"field name: %s, inner column name %s.", jdbcTypeName, expectedTypeInfo, logicalType, fieldName, innerName));
+		}
+	}
+
+	private static Map<String, Class<? extends LogicalType>> getJdbcToFlinkType() {
+		Map<String, Class<? extends LogicalType>> map = new HashMap<>();
+		map.put(CHAR, CharType.class);
+		map.put(VARCHAR, VarCharType.class);
+		map.put(TEXT, VarCharType.class);
+		map.put(TINYINT, TinyIntType.class);
+		map.put(SMALLINT, SmallIntType.class);
+		map.put(INT, IntType.class);
+		map.put(BIGINT, BigIntType.class);
+		map.put(REAL, FloatType.class);
+		map.put(FLOAT, FloatType.class);
+		map.put(DOUBLE, DoubleType.class);
+		map.put(DATE, DateType.class);
+		map.put(TIME, TimeType.class);
+		map.put(TIMESTAMP, TimestampType.class);
+		map.put(DATETIME, TimestampType.class);
+		return map;
+	}
+
+	/**
+	 * Create converter for 'value' columns.
+	 */
+	private static RuntimeConverter createValueConverter(LogicalType logicalType, String realFiledName) {
+		if (!VALUE_COLUMN.equals(realFiledName)) {
+			// return the origin value for columns but 'value' column.
+			return null;
+		}
+		if (logicalType instanceof VarCharType) {
+			return o -> o;
+		} if (logicalType instanceof CharType) {
+			return o -> o;
+		} else if (logicalType instanceof TinyIntType) {
+			return o -> Byte.valueOf((String) o);
+		} else if (logicalType instanceof SmallIntType) {
+			return o -> Short.valueOf((String) o);
+		} else if (logicalType instanceof IntType) {
+			return o -> Integer.valueOf((String) o);
+		} else if (logicalType instanceof BigIntType) {
+			return o -> Long.valueOf((String) o);
+		} else if (logicalType instanceof FloatType) {
+			return o -> Float.valueOf((String) o);
+		} else if (logicalType instanceof DoubleType) {
+			return o -> Double.valueOf((String) o);
+		} else if (logicalType instanceof DateType) {
+			return o -> Date.valueOf((String) o);
+		} else if (logicalType instanceof TimeType) {
+			return o -> Time.valueOf((String) o);
+		} else if (logicalType instanceof TimestampType) {
+			// Timestamp.valueOf will throw an exception if the value of o is
+			// equal to ZERO_TIMESTAMP_STR, so we made a judgment to handle this.
+			return o -> ZERO_TIMESTAMP_STR.equals(o) ? new Timestamp(0) : Timestamp.valueOf((String) o);
+		} else {
+			throw new IllegalArgumentException(
+				String.format("Unsupported type for 'value' column: %s.", logicalType));
+		}
+	}
+
+	private static String removeColumnNamePrefix(String columnName) {
+		if (columnName == null) {
+			return null;
+		}
+		if (columnName.startsWith(BEFORE_PREFIX)) {
+			return columnName.substring(BEFORE_PREFIX.length());
+		} else if (columnName.startsWith(AFTER_PREFIX)) {
+			return columnName.substring(AFTER_PREFIX.length());
+		}
+		return columnName;
+	}
+
+	private static Map<String, Descriptors.FieldDescriptor> initFieldDescriptors() {
+		Map<String, Descriptors.FieldDescriptor> fieldDescriptorMap = new HashMap<>();
+
+		fieldDescriptorMap.put(HEADER, DRCEntry.Entry.getDescriptor().findFieldByName(HEADER));
+		fieldDescriptorMap.put(BODY, DRCEntry.Entry.getDescriptor().findFieldByName(BODY));
+		fieldDescriptorMap.put(BEFORE_IMAGE, DRCEntry.RowData.getDescriptor().findFieldByName(BEFORE_IMAGE));
+		fieldDescriptorMap.put(AFTER_IMAGE, DRCEntry.RowData.getDescriptor().findFieldByName(AFTER_IMAGE));
+		fieldDescriptorMap.put(SQL_TYPE_COLUMN, DRCEntry.Column.getDescriptor().findFieldByName(SQL_TYPE_COLUMN));
+		fieldDescriptorMap.put(NAME_COLUMN, DRCEntry.Column.getDescriptor().findFieldByName(NAME_COLUMN));
+		fieldDescriptorMap.put(NULL_COLUMN, DRCEntry.Column.getDescriptor().findFieldByName(NULL_COLUMN));
+		fieldDescriptorMap.put(VALUE_COLUMN, DRCEntry.Column.getDescriptor().findFieldByName(VALUE_COLUMN));
+		fieldDescriptorMap.put(PAYLOAD, DRCEntry.Message.getDescriptor().findFieldByName(PAYLOAD));
+		fieldDescriptorMap.put(TABLE, DRCEntry.EntryHeader.getDescriptor().findFieldByName(TABLE));
+		fieldDescriptorMap.put(ROWDATAS, DRCEntry.EntryBody.getDescriptor().findFieldByName(ROWDATAS));
+
+		return fieldDescriptorMap;
+	}
+
+	private static Map<String, Descriptors.Descriptor> initDescriptors() {
+		Map<String, Descriptors.Descriptor> descriptorMap = new HashMap<>();
+
+		descriptorMap.put(MESSAGE, DRCEntry.Message.getDescriptor());
+		descriptorMap.put(ENTRY, DRCEntry.Entry.getDescriptor());
+		descriptorMap.put(HEADER, DRCEntry.EntryHeader.getDescriptor());
+		descriptorMap.put(BODY, DRCEntry.EntryBody.getDescriptor());
+
+		return descriptorMap;
+	}
+
+	/**
+	 * Transform column list to name-column map.
+	 */
+	private static Map<String, DynamicMessage> parseAllColumns(
+		List<DynamicMessage> msgList,
+		Descriptors.FieldDescriptor nameFieldDescriptor) {
+		if (msgList == null || msgList.isEmpty()) {
+			return Collections.emptyMap();
+		}
+		Map<String, DynamicMessage> map = new HashMap<>();
+		for (DynamicMessage msg : msgList) {
+			map.put((String) msg.getField(nameFieldDescriptor), msg);
+		}
+		return map;
+	}
+
+	interface RuntimeConverter extends Serializable {
+		Object convert(Object o);
+	}
+}
