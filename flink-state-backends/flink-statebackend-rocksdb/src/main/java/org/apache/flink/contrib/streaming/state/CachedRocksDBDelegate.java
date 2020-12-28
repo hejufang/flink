7c8a3a5a132b1c1b48c72bf8b36f3da7ac0578ad
李明 <liming.1018@bytedance.com>
2020-12-28 07:37:21 +0000

    [INFOI-17572] Redefine the relevant metrics of RocksDBStateBackend cache. [skipped]

--- flink-state-backends/flink-statebackend-rocksdb/src/main/java/org/apache/flink/contrib/streaming/state/CachedRocksDBDelegate.java
+++ flink-state-backends/flink-statebackend-rocksdb/src/main/java/org/apache/flink/contrib/streaming/state/CachedRocksDBDelegate.java
@@ -27,6 +27,7 @@ import org.apache.flink.shaded.guava18.com.google.common.cache.CacheBuilder;
 import org.apache.flink.shaded.guava18.com.google.common.cache.CacheLoader;
 import org.apache.flink.shaded.guava18.com.google.common.cache.CacheStats;
 import org.apache.flink.shaded.guava18.com.google.common.cache.LoadingCache;
+import org.apache.flink.shaded.guava18.com.google.common.cache.RemovalListener;
 
 import org.rocksdb.ColumnFamilyHandle;
 import org.rocksdb.RocksDB;
@@ -37,6 +38,7 @@ import org.slf4j.LoggerFactory;
 
 import java.util.Arrays;
 import java.util.Objects;
+import java.util.concurrent.atomic.AtomicLong;
 
 
 /**
@@ -49,10 +51,20 @@ public class CachedRocksDBDelegate extends AbstractRocksDBDelegate {
 	private static final String CACHE_MISS_COUNT = "cacheMissCount";
 	private static final String CACHE_REQUEST_COUNT = "cacheRequestCount";
 	private static final String CACHE_EVICT_COUNT = "cacheEvictCount";
+	private static final String CACHE_DELETE_COUNT = "cacheDeleteCount";
 	private static final String CACHE_LOAD_TIME = "cacheLoadTime";
 	private static final String CACHE_HIT_RATE = "cacheHitRate";
 
+	private static final String CACHE_EVICT_WITH_WRITE_DISK_COUNT = "cacheEvictWithWriteDiskCount";
+	private static final String CACHE_LOAD_WITH_INVALID_DATA_COUNT = "cacheLoadWithInvalidDataCount";
+	private static final String TOTAL_DISK_OPERATION_COUNT = "totalDiskOperationCount";
+
 	private final LoadingCache<CacheEntry, Boolean> cache;
+
+	private final AtomicLong totalRequestCount = new AtomicLong(0);
+	private final AtomicLong totalDeleteCount = new AtomicLong(0);
+	private final AtomicLong evictWithWriteDiskCount =  new AtomicLong(0);
+	private final AtomicLong loadWithInvalidDataCount = new AtomicLong(0);
 
 	private long lastHitCount;
 	private long lastRequestCount;
@@ -63,12 +75,17 @@ public class CachedRocksDBDelegate extends AbstractRocksDBDelegate {
 		CacheLoader<CacheEntry, Boolean> cacheLoader = new CacheLoader<CacheEntry, Boolean>() {
 			@Override
 			public Boolean load(CacheEntry cacheEntry) throws Exception {
-				return true;
+				return cacheEntry.needUpdate;
 			}
 		};
 		this.cache = CacheBuilder.newBuilder()
 			.maximumSize(maxCacheSize)
 			.recordStats()
+			.removalListener((RemovalListener<CacheEntry, Boolean>) removalNotification -> {
+				if (removalNotification.wasEvicted() && removalNotification.getValue()) {
+					evictWithWriteDiskCount.incrementAndGet();
+				}
+			})
 			.build(cacheLoader);
 		registerMetrics(metricGroup, cache);
 	}
@@ -76,14 +93,18 @@ public class CachedRocksDBDelegate extends AbstractRocksDBDelegate {
 	private void registerMetrics(MetricGroup metricGroup, final Cache cache) {
 		metricGroup.gauge(CACHE_HIT_COUNT, (Gauge<Long>) () -> cache.stats().hitCount());
 		metricGroup.gauge(CACHE_MISS_COUNT, (Gauge<Long>) () -> cache.stats().missCount());
-		metricGroup.gauge(CACHE_REQUEST_COUNT, (Gauge<Long>) () -> cache.stats().requestCount());
+		metricGroup.gauge(CACHE_REQUEST_COUNT, (Gauge<Long>) () -> totalRequestCount.get());
 		metricGroup.gauge(CACHE_EVICT_COUNT, (Gauge<Long>) () -> cache.stats().evictionCount());
 		metricGroup.gauge(CACHE_LOAD_TIME, (Gauge<Long>) () -> cache.stats().totalLoadTime());
+		metricGroup.gauge(CACHE_DELETE_COUNT, (Gauge<Long>) totalDeleteCount::get);
+		metricGroup.gauge(CACHE_EVICT_WITH_WRITE_DISK_COUNT, (Gauge<Long>) evictWithWriteDiskCount::get);
+		metricGroup.gauge(CACHE_LOAD_WITH_INVALID_DATA_COUNT, (Gauge<Long>) loadWithInvalidDataCount::get);
+		metricGroup.gauge(TOTAL_DISK_OPERATION_COUNT, (Gauge<Long>) () -> evictWithWriteDiskCount.get() + cache.stats().missCount() + totalDeleteCount.get());
 		metricGroup.gauge(CACHE_HIT_RATE, (Gauge<Double>) () -> {
 			CacheStats stats = cache.stats();
 			long hitCount = stats.hitCount();
-			long requestCount = stats.requestCount();
-			if (requestCount > 0) {
+			long requestCount = totalRequestCount.get();
+			if (requestCount - lastRequestCount > 0) {
 				double hitRate = (double) (hitCount - lastHitCount) / (requestCount - lastRequestCount);
 				lastHitCount = hitCount;
 				lastRequestCount = requestCount;
@@ -95,30 +116,63 @@ public class CachedRocksDBDelegate extends AbstractRocksDBDelegate {
 
 	@Override
 	public void put(ColumnFamilyHandle columnFamilyHandle, WriteOptions writeOpts, byte[] key, byte[] value) throws RocksDBException {
-		visitCache(new CacheEntry(columnFamilyHandle, key));
+		totalRequestCount.incrementAndGet();
+		updateCache(new CacheEntry(columnFamilyHandle, key));
 		delegate.put(columnFamilyHandle, writeOpts, key, value);
 	}
 
 	@Override
 	public byte[] get(ColumnFamilyHandle columnFamilyHandle, byte[] key) throws RocksDBException {
-		visitCache(new CacheEntry(columnFamilyHandle, key));
-		return delegate.get(columnFamilyHandle, key);
+		totalRequestCount.incrementAndGet();
+		CacheEntry entry = new CacheEntry(columnFamilyHandle, key);
+		byte[] value = delegate.get(columnFamilyHandle, key);
+		if (value == null) {
+			visitCache(entry, false);
+			loadWithInvalidDataCount.incrementAndGet();
+		} else {
+			visitCache(entry, true);
+		}
+		return value;
 	}
 
 	@Override
 	public void delete(ColumnFamilyHandle columnFamilyHandle, WriteOptions writeOpt, byte[] key) throws RocksDBException {
+		totalRequestCount.incrementAndGet();
+		totalDeleteCount.incrementAndGet();
+		invalidCache(new CacheEntry(columnFamilyHandle, key));
 		delegate.delete(columnFamilyHandle, writeOpt, key);
 	}
 
 	@Override
 	public void merge(ColumnFamilyHandle columnFamilyHandle, WriteOptions writeOpts, byte[] key, byte[] value) throws RocksDBException {
-		visitCache(new CacheEntry(columnFamilyHandle, key));
+		totalRequestCount.incrementAndGet();
+		visitCache(new CacheEntry(columnFamilyHandle, key, true), true);
 		delegate.merge(columnFamilyHandle, writeOpts, key, value);
 	}
 
-	private void visitCache(CacheEntry cacheEntry) {
+	private void visitCache(CacheEntry cacheEntry, boolean load) {
 		try {
-			cache.get(cacheEntry);
+			if (load) {
+				cache.get(cacheEntry);
+			} else {
+				cache.getIfPresent(cacheEntry);
+			}
+		} catch (Throwable ignore) {
+			// ignore
+		}
+	}
+
+	private void updateCache(CacheEntry cacheEntry) {
+		try {
+			cache.put(cacheEntry, true);
+		} catch (Throwable ignore) {
+			// ignore
+		}
+	}
+
+	private void invalidCache(CacheEntry cacheEntry) {
+		try {
+			cache.invalidate(cacheEntry);
 		} catch (Throwable ignore) {
 			// ignore
 		}
@@ -127,10 +181,16 @@ public class CachedRocksDBDelegate extends AbstractRocksDBDelegate {
 	private static class CacheEntry {
 		private final ColumnFamilyHandle columnFamilyHandle;
 		private final byte[] key;
+		private final boolean needUpdate;
 
 		public CacheEntry(ColumnFamilyHandle columnFamilyHandle, byte[] key) {
+			this(columnFamilyHandle, key, false);
+		}
+
+		public CacheEntry(ColumnFamilyHandle columnFamilyHandle, byte[] key, boolean needUpdate) {
 			this.columnFamilyHandle = columnFamilyHandle;
 			this.key = key;
+			this.needUpdate = needUpdate;
 		}
 
 		@Override
