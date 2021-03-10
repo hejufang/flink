2905b444a72f25e61fd4cc4d5f1213db1e6404fa
乐言 <leyan.orange@bytedance.com>
2021-03-10 13:40:39 +0800

    [INFOI-19978] Limit max mem used for ci

--- build_local.sh
+++ build_local.sh
@@ -20,7 +20,9 @@
 set -eo pipefail
 
 . /etc/profile
+
 export JAVA_HOME=/usr/local/jdk
+export MAVEN_OPTS=-Xmx1024m
 
 mvn clean install -DskipTests -T 1C -Pinclude-hadoop -Psql-jars -Pdocs-and-source
 mvn clean test -Dsurefire.rerunFailingTestsCount=3 -Dflink.forkCount=2 -Dflink.forkCountTestPackage=2 -pl flink-runtime,flink-core,flink-metrics,flink-table/flink-sql-parser,flink-table/flink-sql-parser-hive,flink-table/flink-table-api-java,flink-table/flink-table-api-java-bridge,flink-table/flink-table-common,flink-table/flink-table-planner-blink,flink-table/flink-table-runtime-blink,flink-table/flink-table-uber-blink,flink-clients
