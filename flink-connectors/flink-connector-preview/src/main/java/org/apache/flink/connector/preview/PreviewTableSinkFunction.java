22eda1eb5452bc9dcc6b7ccd0495b9daee3493b8
庞司坦 <pangsitan@bytedance.com>
2022-06-14 11:26:42 +0000

    [#5238709] limit preview result/changelog size by akka.framesize

--- flink-connectors/flink-connector-preview/src/main/java/org/apache/flink/connector/preview/PreviewTableSinkFunction.java
+++ flink-connectors/flink-connector-preview/src/main/java/org/apache/flink/connector/preview/PreviewTableSinkFunction.java
@@ -21,20 +21,23 @@ import org.apache.flink.api.common.io.ratelimiting.FlinkConnectorRateLimiter;
 import org.apache.flink.api.common.serialization.SerializationSchema;
 import org.apache.flink.api.common.typeinfo.TypeInformation;
 import org.apache.flink.api.common.typeutils.TypeSerializer;
+import org.apache.flink.configuration.AkkaOptions;
 import org.apache.flink.configuration.Configuration;
+import org.apache.flink.configuration.MemorySize;
 import org.apache.flink.runtime.rest.messages.taskmanager.preview.PreviewDataRequest;
 import org.apache.flink.runtime.rest.messages.taskmanager.preview.PreviewDataResponse;
 import org.apache.flink.streaming.api.functions.SpecificParallelism;
 import org.apache.flink.streaming.api.functions.sink.PreviewSinkFunction;
 import org.apache.flink.table.data.RowData;
 import org.apache.flink.types.RowKind;
+import org.apache.flink.util.FlinkRuntimeException;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -51,8 +54,8 @@ public class PreviewTableSinkFunction extends PreviewSinkFunction<RowData> imple
 	private final TypeInformation<RowData> rowDataTypeInformation;
 	private final FlinkConnectorRateLimiter rateLimiter;
 
-	private transient LinkedList<RowData> tableResult;
-	private transient LinkedList<String> changeLogResult;
+	private transient ResultDataManager<RowData> tableResult;
+	private transient ResultDataManager<String> changeLogResult;
 	private transient TypeSerializer<RowData> typeSerializer;
 
 	public PreviewTableSinkFunction(
@@ -68,12 +71,31 @@ public class PreviewTableSinkFunction extends PreviewSinkFunction<RowData> imple
 	@Override
 	public void open(Configuration parameters) throws Exception {
 		super.open(parameters);
-		changeLogResult = new LinkedList<>();
-		tableResult = new LinkedList<>();
+		initResultSizeSpace(MemorySize.parse(parameters.get(AkkaOptions.FRAMESIZE)));
 		typeSerializer = rowDataTypeInformation.createSerializer(getRuntimeContext().getExecutionConfig());
 		if (rateLimiter != null) {
 			rateLimiter.open(getRuntimeContext());
 		}
+	}
+
+	private void initResultSizeSpace(MemorySize akkaFrameSize){
+		/*
+		 * left 10% buffer.
+		 */
+		double maxByteSize = akkaFrameSize.getBytes() * 0.9;
+		long maxResultByteSize;
+		if (previewTableOptions.isChangelogModeEnable() && previewTableOptions.isTableModeEnable()) {
+			/*
+			 * tableResult and changeLog will take each half the space.
+			 */
+			maxResultByteSize = new Double(maxByteSize / 2).longValue();
+		} else {
+			maxResultByteSize = new Double(maxByteSize).longValue();
+		}
+		LOG.info("the preview result byteSize max is: {}", maxResultByteSize);
+		changeLogResult = new ResultDataManager<>(previewTableOptions.getChangelogRowsMax(), maxResultByteSize, null);
+		tableResult = new ResultDataManager<>(previewTableOptions.getTableRowsMax(), maxResultByteSize,
+			serializationSchema);
 	}
 
 	@Override
@@ -81,36 +103,29 @@ public class PreviewTableSinkFunction extends PreviewSinkFunction<RowData> imple
 		synchronized (this) {
 			PreviewDataResponse previewDataResponse = new PreviewDataResponse();
 			if (previewTableOptions.isChangelogModeEnable()) {
-				previewDataResponse.setChangeLogResult(changeLogResult);
+				previewDataResponse.setChangeLogResult(changeLogResult.getResultList());
 			}
 			if (previewTableOptions.isTableModeEnable()) {
-				previewDataResponse.setTableResult(getResultByRow(tableResult));
+				previewDataResponse.setTableResult(getResultByRow(tableResult.getResultList()));
 			}
 			LOG.info("previewFunction return data, changeLogResult size: {}, tableResult size: {}",
-				changeLogResult.size(), tableResult.size());
+				changeLogResult.getResultList().size(), tableResult.getResultList().size());
 			return previewDataResponse;
 		}
 	}
 
 	private List<String> getResultByRow(Collection<RowData> rowDataCollection) {
-		List<String> result = new ArrayList<>();
-		Iterator<RowData> rowDataIterable =  rowDataCollection.iterator();
-		while (rowDataIterable.hasNext()) {
-			RowData rowData = rowDataIterable.next();
+		List<String> result = new ArrayList<>(rowDataCollection.size());
+		for (RowData rowData : rowDataCollection) {
 			result.add(new String(serializationSchema.serialize(rowData)));
 		}
 		return result;
 	}
 
 	private void processChangeLogResult(RowData rowData) {
-		if (changeLogResult.size() >= previewTableOptions.getChangelogRowsMax()) {
-			changeLogResult.removeFirst();
-		}
+		final String data = rowData.getRowKind().shortString() + new String(serializationSchema.serialize(rowData));
 		// serialize first for performance and table result will update rowKind insert
-		changeLogResult.add(new StringBuilder()
-			.append(rowData.getRowKind().shortString())
-			.append(new String(serializationSchema.serialize(rowData)))
-			.toString());
+		changeLogResult.addData(data);
 	}
 
 	private void processTableResult(RowData rowData) {
@@ -124,18 +139,16 @@ public class PreviewTableSinkFunction extends PreviewSinkFunction<RowData> imple
 
 		// insert
 		if (isInsertOp) {
-			if (tableResult.size() >= previewTableOptions.getTableRowsMax()) {
-				tableResult.removeFirst();
-			}
-			tableResult.add(rowData);
+			tableResult.addData(rowData);
 		}
 		// delete
 		else {
 			// TODO update performance for avoid all traverse
 			// delete first duplicate rowData
-			for (int i = tableResult.size() - 1; i >= 0; i--) {
-				if (tableResult.get(i).equals(rowData)) {
-					tableResult.remove(i);
+			for (int i = tableResult.getResultList().size() - 1; i >= 0; i--) {
+				if (tableResult.getResultList().get(i).equals(rowData)) {
+					final RowData remove = tableResult.getResultList().remove(i);
+					tableResult.releaseSpace(remove);
 					break;
 				}
 			}
@@ -180,5 +193,83 @@ public class PreviewTableSinkFunction extends PreviewSinkFunction<RowData> imple
 	@Override
 	public int getParallelism() {
 		return 1;
+	}
+
+	private static class ResultDataManager<T>{
+		private final transient LinkedList<T> resultList;
+		/**
+		 * max number of result list.
+		 */
+		private final transient int maxResultCount;
+		private final transient long maxResultByteSize;
+		private final transient SerializationSchema<T> serializationSchema;
+
+		private transient long currentResultByteSize;
+
+		public ResultDataManager(int maxResultCount, long maxResultByteSize,
+			SerializationSchema<T> serializationSchema) {
+			this.resultList = new LinkedList<>();
+			this.maxResultCount = maxResultCount;
+			this.currentResultByteSize = 0L;
+			this.maxResultByteSize = maxResultByteSize;
+			this.serializationSchema = serializationSchema;
+		}
+
+		public void addData(T data){
+			final int byteSize = getDataByteSize(data);
+			while (!applyResultSizeSpace(byteSize)){
+				releaseSpace(resultList.removeFirst());
+			}
+			resultList.add(data);
+		}
+
+		/**
+		 * Apply for space.
+		 * @return if it's able to add new data. false mean we need to remove old data.
+		 */
+		private boolean applyResultSizeSpace(int byteSize) {
+			if (resultList.size() >= this.maxResultCount){
+				return false;
+			}
+			if (byteSize >= this.maxResultByteSize){
+				final String msg = String.format("The size of a single record exceeds the maximum limit. "
+					+ "Record size: %s, maxResultSize: %s", byteSize, this.maxResultByteSize);
+				throw new FlinkRuntimeException(msg);
+			}
+			if (this.currentResultByteSize + byteSize >= this.maxResultByteSize) {
+				LOG.debug("space is full. totalBytes: {}, content length: {}", this.currentResultByteSize, byteSize);
+				return false;
+			}
+			this.currentResultByteSize += byteSize;
+			return true;
+		}
+
+		/**
+		 * release space.
+		 */
+		private void releaseSpace(T data) {
+			this.currentResultByteSize -= getDataByteSize(data);
+		}
+
+		/**
+		 * convert data to string and get byte size.
+		 */
+		private int getDataByteSize(T data){
+			String content;
+			if (String.class.equals(data.getClass())){
+				content = (String) data;
+			} else {
+				if (serializationSchema != null){
+					content = new String(serializationSchema.serialize(data));
+				} else {
+					throw new FlinkRuntimeException("serializationSchema is null");
+				}
+			}
+			return content.getBytes(StandardCharsets.UTF_8).length;
+		}
+
+		public LinkedList<T> getResultList() {
+			return resultList;
+		}
 	}
 }
